c7fc99133efe15f333b1429733944ca41313c1f6
Ferullo <ferullo@wsip-70-164-71-203.ok.ok.cox.net>
2022-02-22 13:52:26 -0600

    Added cryptographic secret key

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use((req,res,next) => {
 })
 
 app.use(sessions({
-    secret: `${Math.random()}`,
+    secret: `${require('crypto').randomBytes(48, function(err, buffer) { var token = buffer.toString('hex'); console.log(token); })}`,
     saveUninitialized: true,
     resave: false
 }))
